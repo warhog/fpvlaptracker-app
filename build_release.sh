56161db2d5ccfac23e6130b755c598fde92b1e08
warhog <547310+warhog@users.noreply.github.com>
2018-07-10 21:09:50 +0200

    bump version to 2.0

--- build_release.sh
+++ build_release.sh
@@ -20,7 +20,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # run zipalignment
-/home/$USER/Android/Sdk/build-tools/26.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk flt.apk
+/home/$USER/Android/Sdk/build-tools/28.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk flt.apk
 if [ $? -ne 0 ]; then
     echo "failed to align zip file"
     exit 1
